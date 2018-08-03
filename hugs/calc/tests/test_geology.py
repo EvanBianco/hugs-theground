18b4c16ea11df89bb5f1f3063ffd9de392ae11a0
Evan Bianco <evan@agilegeoscience.com>
2018-08-03 15:19:01 -0300

    Add more tests

--- hugs/calc/tests/test_geology.py
+++ hugs/calc/tests/test_geology.py
@@ -3,9 +3,18 @@
 from hugs.calc import snell_angle
 
 from numpy.testing import assert_almost_equal
-
+import pytest
 
 def test_snell():
     """Test the basic wind component calculation."""
     res = snell_angle(12, 2500, 4000)
     assert_almost_equal(res, 19.43022, 4)
+
+def test_snell_zero_velocity_top():
+    """Test to ensure that a ValueError is raised 
+    on the `snell_angle` function when the top
+    layer velocity is zero"""
+    with pytest.raises(ValueError):
+        snell_angle(30,0,2000) 
+
+
--- hugs/calc/tests/test_met.py
+++ hugs/calc/tests/test_met.py
@@ -1,10 +1,10 @@
 """Test the `met` module."""
 
-from hugs.calc import get_wind_dir, get_wind_speed
+from hugs.calc import get_wind_dir, get_wind_speed, get_wind_components
 
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
-
+import pytest
 
 def test_speed():
     """Test calculating wind speed."""
@@ -35,3 +35,28 @@ def test_dir():
     true_dir = np.array([270., 225., 180., 270.])
 
     assert_array_almost_equal(true_dir, direc, 4)
+
+def test_wind_components_scalar():
+    """Test calculating wind components using scalar inputs"""
+    s = 8.0
+    direc = 0
+    u, v = get_wind_components(s,direc)
+    print(u,v)
+    assert_array_almost_equal(u, -0.0, 3)
+    assert_array_almost_equal(v, -8.0, 3)
+
+def test_wind_components_array():
+    """Test calculating wind components using array inputs"""
+    s = np.array([10.0, 10.0, 10.0])
+    direc = np.array([0, 45, 90]) 
+    u, v = get_wind_components(s,direc)
+    print(u)
+    true_u = np.array([-0.0, -7.071, -10.0])
+    true_v = np.array([-10.0, -7.071, 0.0])
+    assert_array_almost_equal(u, true_u, 3)
+    assert_array_almost_equal(v, true_v, 3)
+
+def test_warning_direction():
+    """Test that warning is raised when wind direction > 360"""
+    with pytest.warns(UserWarning):
+        get_wind_components(3,480)
